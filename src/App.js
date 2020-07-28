86661bf8565137ccf43b1f11691fe500c410228d
emanoelcominesi <emanoel.cominesi@kmm.local>
2020-07-28 01:50:06 -0300

    ajuste pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div>
       <Menu />
       
-      
+      <BannerMain
+         videoTitle = {dadosIniciais.categorias[0].videos[0].titulo}
+         url = {dadosIniciais.categorias[0].videos[0].url}
+         videoDescription = {"Titulo!"}
+      />
+
+      <Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[0]}
+      />
+
+      <Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[1]}
+      />
+
+      <Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[2]}
+      />
+
+<Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[3]}
+      />
+
+      <Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[4]}
+      />
+
+      <Carousel 
+         ignoreFirstVideo
+         category = {dadosIniciais.categorias[5]}
+      />
     </div>
   );
 }
